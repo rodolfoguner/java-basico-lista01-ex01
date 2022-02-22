b906a3349332c16d579f8a38fd6dc225462fa059
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-21 21:53:02 -0300

    feat: adicionado validacao e operacoes de idade

--- src/br/fai/ex01/Main.java
+++ src/br/fai/ex01/Main.java
@@ -8,6 +8,16 @@ public class Main {
 	}
 	
 	private void start() {
-		
+		int idade = 15;
+		System.out.println("A idade é de " + idade + " anos");
+		 idade = (idade + 5) * 2;
+		System.out.println("Resultado 1: " + idade);
+		idade -= 5;
+		System.out.println("Resultado 2: " + idade);
+		if (idade > 20) {
+			System.out.println("A idade é maior que 20");
+		} else {
+			System.out.println("A idade é menor que 20");
+		}
 	}
 }
